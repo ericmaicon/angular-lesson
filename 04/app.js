fcc9dcfea5f65dd80db239ad0a69df9e93bb83eb
Eric Maicon <eric@ericmaicon.com.br>
2015-08-03 16:16:20 -0300

    lesson 04

---
+++ 04/app.js
@@ -0,0 +1,84 @@
+(function() {
+    var app = angular.module('store', []);
+
+    app.controller('StoreController', function() {
+        this.products = [
+            {
+                name: 'Dodecahedron',
+                price: 2.95,
+                description: ' . . .',
+                images: [
+                    'images/test-full.png',
+                    'images/test-thumb.png'
+                ],
+                reviews: [
+                    {
+                        stars: 5,
+                        body: "I love this product!",
+                        author: 'joe@thomas.com'
+                    },
+                    {
+                        stars: 3,
+                        body: "I don't love this product!",
+                        author: 'jack@thomas.com'
+                    }
+                ]
+            },
+            {
+                name: 'Pentagonal Gem',
+                price: 5.95,
+                description: ' . . .',
+                images: [
+                    'images/test-full.png',
+                    'images/test-thumb.png'
+                ],
+                reviews: [
+                    {
+                        stars: 5,
+                        body: "I love this product!",
+                        author: 'joe@thomas.com'
+                    }
+                ]
+            }
+        ];
+    });
+
+    app.controller('TabController', function(){
+        this.tab = 1;
+
+        this.setTab = function(newValue){
+            this.tab = newValue;
+        };
+
+        this.isSet = function(tabName){
+            return this.tab === tabName;
+        };
+    });
+
+    app.controller('ReviewController', function() {
+        this.review = {};
+
+        this.addReview = function(product) {
+            product.reviews.push(this.review);
+            this.review = {};
+        }
+    });
+
+    app.directive('productTitle', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'product-title.html'
+        };
+    });
+
+    app.directive('productPanels', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'product-panels.html',
+            controller: function() {
+
+            },
+            controllerAs: 'panels'
+        };
+    });
+})();
