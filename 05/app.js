13245c4ee743006e438e32998dc34a7de47eabb6
Eric Maicon <eric@ericmaicon.com.br>
2015-08-03 16:30:32 -0300

    finishing 05 lesson

--- 05/app.js
+++ 05/app.js
@@ -1,47 +1,14 @@
 (function() {
     var app = angular.module('store', ['store-products']);
 
-    app.controller('StoreController', function() {
-        this.products = [
-            {
-                name: 'Dodecahedron',
-                price: 2.95,
-                description: ' . . .',
-                images: [
-                    'images/test-full.png',
-                    'images/test-thumb.png'
-                ],
-                reviews: [
-                    {
-                        stars: 5,
-                        body: "I love this product!",
-                        author: 'joe@thomas.com'
-                    },
-                    {
-                        stars: 3,
-                        body: "I don't love this product!",
-                        author: 'jack@thomas.com'
-                    }
-                ]
-            },
-            {
-                name: 'Pentagonal Gem',
-                price: 5.95,
-                description: ' . . .',
-                images: [
-                    'images/test-full.png',
-                    'images/test-thumb.png'
-                ],
-                reviews: [
-                    {
-                        stars: 5,
-                        body: "I love this product!",
-                        author: 'joe@thomas.com'
-                    }
-                ]
-            }
-        ];
-    });
+    app.controller('StoreController', ['$http', function($http) {
+        var store = this;
+        store.products = [];
+
+        $http.get('/products.json').success(function(data) {
+            store.products = data;
+        });
+    }]);
 
     app.controller('ReviewController', function() {
         this.review = {};
